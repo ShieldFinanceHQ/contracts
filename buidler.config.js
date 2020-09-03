037a4afcb2894b3cd94e6cec9878f159b469107d
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2020-09-03 20:11:17 +0700

    Enable optimizer

--- buidler.config.js
+++ buidler.config.js
@@ -36,7 +36,8 @@ module.exports = {
     }
   },
   solc: {
-    version: '0.6.12'
+    version: '0.6.12',
+    optimizer: { enabled: true, runs: 200 }
   },
   spdxLicenseIdentifier: {
     overwrite: false,
